990c3d57cb8a8bb289a2c7845cf520a21422c289
KaseyPasqualini <kaseypasqualini@yahoo.com>
2017-10-18 18:15:35 -0400

    complete

---
+++ app/models/article.rb
@@ -0,0 +1,12 @@
+# class Article < ApplicationRecord
+#   has_many :comments
+#   validates :title, presence: true,
+#                     length: { minimum: 5 }
+# end
+
+
+class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+end
